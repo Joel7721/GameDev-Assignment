51d4ed972c12d2f5529c179638b31f10e9ff3f1d
Joel7721 <mensahjl@mail.uc.edu>
2024-05-19 23:20:10 -0400

    Initial commit

---
+++ Assets/Player.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+    public Rigidbody2D rb;
+
+    public float movingInput;
+
+    public LayerMask whatIsGround;
+    public float groundCheckDistance;
+    private bool isGrounded;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        CollisionCheck();
+
+        movingInput = Input.GetAxisRaw("Horizontal");
+
+        Move();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded)
+            {
+                Jump();
+
+            }
+        }
+    }
+
+    private void CollisionCheck()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(moveSpeed * movingInput, rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Trigger")
+        {
+            Debug.Log("Trigger");
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - groundCheckDistance));
+    }
+}
